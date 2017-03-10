004e556944f8dc76cfe4044bfbe86fd350c91d7a
Sara Tan <saratan@mac.com>
2017-03-09 23:47:00 -0800

    Create components for home page

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,51 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  NavigatorIOS,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-export default class SafeSpaces extends Component {
+class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={styles.text}>
+          Welcome to Safe Spaces!
         </Text>
       </View>
     );
   }
 }
 
+class SafeSpaces extends Component {
+
+  render() {
+    return (
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Safe Spaces',
+          component: Home,
+        }}/>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  },
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('SafeSpaces', () => SafeSpaces);
