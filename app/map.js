1d6a6fd85ab47f89a4bd8386cfcb8d3e67e6d539
Erik Hauner <ehauner@gmail.com>
2017-03-11 12:38:28 -0800

    Added a marker

--- app/map.js
+++ app/map.js
@@ -1,13 +1,45 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { MapView, StyleSheet } from 'react-native';
+import { MapView, StyleSheet, Dimensions, Geolocation } from 'react-native';
+
+var { width, height } = Dimensions.get('window');
+const ASPECT_RATIO = width / height;
+
+// (Initial Static Location) Vancouver
+const LATITUDE = 49.282729;
+const LONGITUDE = -123.120738;
+const LATITUDE_DELTA = 0.01;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+var markers = [
+  {
+    latitude: LATITUDE,
+    longitude: LONGITUDE,
+    title: 'Foo Place',
+    subtitle: '1234 Foo Drive'
+  }
+];
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapRegion: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+    }
+  }
+
   render() {
     return (
-      <MapView style={styles.container}
-        showsUserLocation={true}
+      <MapView
+        style={styles.map}
+        region={this.state.mapRegion}
+        annotations={markers}
       />
     );
   }
@@ -20,7 +52,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#656565'
   },
-  container: {
+  map: {
     flex: 1
   }
 });
