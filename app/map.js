4e3cbfc413bb400e07de045ec99ccc31391df2ba
Erik Hauner <ehauner@gmail.com>
2017-03-14 23:16:35 -0700

    Added ability to place a new draggable marker

--- app/map.js
+++ app/map.js
@@ -30,30 +30,63 @@ class Map extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mapRegion: {
-        latitude: 49.282799,
-        longitude: -123.120768,
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
         latitudeDelta: LATITUDE_DELTA,
         longitudeDelta: LONGITUDE_DELTA,
       },
       restrooms: null,
+      addingRestroom: false,
+      newRestroomCoordinate: null,
     }
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
 
   componentWillMount() {
     getRestrooms(this);
   }
 
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
+  addRestroom() {
+    this.setState({
+      addingRestroom: true,
+      newRestroomCoordinate: this.state.region,
+    });
+  }
+
   render() {
+
+    // Don't render if the restrooms haven't been loaded
     if (this.state.restrooms == null)
       return null;
+
+    // Place a restroom marker if user is adding a restroom
+    let newRestroomMarker = null;
+    if (this.state.addingRestroom) {
+      newRestroomMarker = (
+        <MapView.Marker draggable
+          coordinate={this.state.newRestroomCoordinate}
+          pinColor={'#0082f9'}
+          onDragEnd={
+            (e) => this.setState({
+              newRestroomCoordinate: e.nativeEvent.coordinate
+            })
+          }
+        />
+      );
+    }
+
     return (
       <View style={styles.container}>
 
         {/* Map that shows user location */}
         <MapView
           style={styles.map}
-          region={this.state.mapRegion}
+          region={this.state.region}
           onRegionChange={this.onRegionChange}
           showsUserLocation={true}
         >
@@ -80,7 +113,17 @@ class Map extends Component {
 
             </MapView.Marker>
           ))}
+
+          {/* Draggable marker for new restroom */}
+          {newRestroomMarker}
+
         </MapView>
+        <Text>{this.state.region.latitude},{this.state.region.longitude}</Text>
+        <TouchableOpacity
+          onPress={()=> this.addRestroom()}
+          activeOpacity={.8}>
+          <Text style={styles.buttonText}>Add a restroom</Text>
+        </TouchableOpacity>
       </View>
     );
   }
